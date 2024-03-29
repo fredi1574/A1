91fafb363b00f3a6dee7b517359a65fad2e859e6
fredi1574 <fredi1574@gmail.com>
2024-03-29 18:46:42 +0300

    Changed the postion of the Theme switcher component

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -6,10 +6,10 @@ const MainLayout = ({ children, backgroundColor }) => {
     <div
       className={`min-h-screen ${backgroundColor} select-none font-fredoka overflow-x-auto`}
     >
-      <div className="absolute md:top-28 md:left-4 top-2 left-2">
+      <div className="absolute md:top-32 md:left-4 top-4 left-4">
         <ThemeSwitcher />
       </div>
-      <main className="flex-grow">{children}</main>
+      <main>{children}</main>
       {/* <Footer /> */}
     </div>
   );
